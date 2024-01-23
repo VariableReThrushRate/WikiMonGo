b1cfa373d08b28047c9c9f24d8e51618d054df8d
bgpil <bgpil@OBSIDIAN>
2024-01-23 15:30:35 -0800

    Add project files.

---
+++ WikiMonGo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WikiMonGo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WikiMonGo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WikiMonGo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
